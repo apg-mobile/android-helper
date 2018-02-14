048ceb805eb7c529401505c5e404a23c73791716
Sattha Puangput <sattha.puangput@gmail.com>
2018-02-14 09:56:40 +0700

    Fixed - Bug  on Samsung galaxy note edge (Android 6.0.1), detect unknown usb in its device ?

--- library/src/main/java/com/apg/library/corehelper/UsbServiceHelper.java
+++ library/src/main/java/com/apg/library/corehelper/UsbServiceHelper.java
@@ -15,6 +15,9 @@ import java.util.Iterator;
 
 public class UsbServiceHelper {
 
+    private final int QUALCOMM_CDMA_PRODUCT_ID = 37022;
+    private final int QUALCOMM_CDMA_VENDOR_ID = 1478;
+
     private UsbManager getUsbManager (Context context) {
         return  (UsbManager) context.getSystemService(Context.USB_SERVICE);
     }
@@ -23,14 +26,18 @@ public class UsbServiceHelper {
 
         HashMap<String, UsbDevice> deviceList = getUsbManager(context).getDeviceList();
 
-        Iterator<UsbDevice> iterator = deviceList.values().iterator();
-        while (iterator.hasNext()) {
-            UsbDevice device = iterator.next();
-            if (device.getDeviceClass() == UsbConstants.USB_CLASS_HID  ||
-                    device.getDeviceClass() == UsbConstants.USB_CLASS_PER_INTERFACE) {
+        for (UsbDevice device : deviceList.values()) {
+            if (device.getDeviceClass() == UsbConstants.USB_CLASS_HID || device.getDeviceClass() == UsbConstants.USB_CLASS_PER_INTERFACE) {
+
+                // Bug on Samsung galaxy note edge (Android 6.0.1), detect unknown usb in its device ?
+                if (device.getProductId() == QUALCOMM_CDMA_PRODUCT_ID && device.getVendorId() == QUALCOMM_CDMA_VENDOR_ID) {
+                    continue;
+                }
+
                 return true;
             }
         }
+
         return false;
     }
 
